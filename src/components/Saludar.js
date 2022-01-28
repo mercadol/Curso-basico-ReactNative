5ecc109eb31baf29b726fd661a7bc83370acaab3
Leonardo Mercado <lamercadob1@gmail.com>
2022-01-28 15:33:07 -0500

    12/16 validacion de propiedades: PropType

--- src/components/Saludar.js
+++ src/components/Saludar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Text } from "react-native";
+import PropTypes from "prop-types"
 
 export default function Saludar(props) {
     const { firstname, lastname }=props;
@@ -7,9 +8,14 @@ export default function Saludar(props) {
         <Text>Hola {firstname} {lastname}</Text>
     );
 }
-
+/*
 Saludar.defaultProps={
     firstname: "leonardo",
     lastname: "mercado"
+};*/
+
+Saludar.propTypes={
+    firstname: PropTypes.string.isRequired,
+    lastname: PropTypes.string
 };
 
